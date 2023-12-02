adabfce068ee72d937edb186c25461aeaf68de57
HyuntaeLee <97331148+hyuntaeLee@users.noreply.github.com>
2023-12-02 18:44:23 +0900

    Update pwndocker.sh

--- pwndocker.sh
+++ pwndocker.sh
@@ -3,12 +3,12 @@
 echo "Running $1 version pwndocker..."
 
 case "$1" in
-	"16.04") docker run -it --rm --cap-add SYS_PTRACE --security-opt seccomp:unconfined --name 16.04 -v $PWD/study:/study pwnbuntu:16.04 /usr/bin/zsh
+	"16.04") docker run -it --rm --cap-add SYS_PTRACE --security-opt seccomp:unconfined --name 16.04 -v $PWD/study:/study leepwn:16.04 /usr/bin/zsh
 	;;
-	"18.04") docker run -it --rm --cap-add SYS_PTRACE --security-opt seccomp:unconfined --name 18.04 -v $PWD/study:/study pwnbuntu:18.04 /usr/bin/zsh
+	"18.04") docker run -it --rm --cap-add SYS_PTRACE --security-opt seccomp:unconfined --name 18.04 -v $PWD/study:/study leepwn:18.04 /usr/bin/zsh
 	;;
-	"20.04") docker run -it --rm --cap-add SYS_PTRACE --security-opt seccomp:unconfined --name 20.04 -v $PWD/study:/study pwnbuntu:20.04 /usr/bin/zsh
+	"20.04") docker run -it --rm --cap-add SYS_PTRACE --security-opt seccomp:unconfined --name 20.04 -v $PWD/study:/study leepwn:20.04 /usr/bin/zsh
 	;;
-	"22.04") docker run -it --rm --cap-add SYS_PTRACE --security-opt seccomp:unconfined --name 22.04 -v $PWD/study:/study pwnbuntu:22.04 /usr/bin/zsh
+	"22.04") docker run -it --rm --cap-add SYS_PTRACE --security-opt seccomp:unconfined --name 22.04 -v $PWD/study:/study leepwn:22.04 /usr/bin/zsh
 	;;
 esac
